0e5e6cd6d0ccffac4d1b1ea1e738148d1670b3b7
Nehaverma43 <neha.verma@microsave.net>
2024-06-07 22:56:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def get_pdf_text(pdf_docs):
 
 def get_text_chunks(text):
     splitter = RecursiveCharacterTextSplitter(
-        chunk_size=10000, chunk_overlap=1000)
+        chunk_size=1000, chunk_overlap=200)
     chunks = splitter.split_text(text)
     return chunks
 
