5f694dd639a7e758c54dcb5dbafe53654df8f2e1
Nehaverma43 <neha.verma@microsave.net>
2024-06-07 17:59:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,9 +11,11 @@ from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
 from langchain.chains.summarize import load_summarize_chain
 from dotenv import load_dotenv
+from google.api_core.exceptions import DeadlineExceeded
+
 logger = logging.getLogger(__name__)
 load_dotenv()
-google_api_key = "AIzaSyDU7L29UsHIHGhoIICcHYtTvDIQZ4pl5lU"
+google_api_key = os.getenv("GOOGLE_API_KEY", "AIzaSyDU7L29UsHIHGhoIICcHYtTvDIQZ4pl5lU")
 google_genai_model = "gemini-pro"
 pdf_folder_path = r"Pdfs"
 
@@ -32,43 +34,46 @@ def get_text_chunks(text):
     return chunks
 
 def get_vector_store(chunks):
-    embeddings = GoogleGenerativeAIEmbeddings(
-        model="models/embedding-001", google_api_key=google_api_key) 
-    vector_store = FAISS.from_texts(chunks, embedding=embeddings)
-    vector_store.save_local("faiss_index")
+    try:
+        embeddings = GoogleGenerativeAIEmbeddings(
+            model="models/embedding-001", google_api_key=google_api_key) 
+        vector_store = FAISS.from_texts(chunks, embedding=embeddings)
+        vector_store.save_local("faiss_index")
+    except DeadlineExceeded:
+        logger.error("Embedding request timed out. Please try again later.")
+        st.error("Embedding request timed out. Please try again later.")
 
 def get_conversational_chain(google_api_key=google_api_key):
     map_prompt = PromptTemplate.from_template(
-    """
-    Write a concise summary of the following:
+        """
+        Write a concise summary of the following:
 
-    "{text}"
+        "{text}"
 
-    CONCISE SUMMARY:
-    """
+        CONCISE SUMMARY:
+        """
     )
-
-    
-
-    model = ChatGoogleGenerativeAI(model=google_genai_model,
-                                   client=genai,
-                                   temperature=0.3,
-                                   google_api_key=google_api_key,)
+    model = ChatGoogleGenerativeAI(
+        model=google_genai_model,
+        client=genai,
+        temperature=0.3,
+        google_api_key=google_api_key
+    )
     chain = load_summarize_chain(
-    llm=model, 
-    chain_type="map_reduce", 
-    map_prompt = map_prompt, 
-    
-    # these variables are the default values and can be modified/omitted
-    combine_document_variable_name="text",
-    map_reduce_document_variable_name="text",
-)
+        llm=model,
+        chain_type="map_reduce",
+        map_prompt=map_prompt,
+        combine_document_variable_name="text",
+        map_reduce_document_variable_name="text",
+    )
     return chain
 
 def HR_Policies():
     st.session_state.messages = [{"role": "assistant", "content": "Ask your queries related to MSC HR Policies"}]
+
 def MSC_publications():
     st.session_state.messages = [{"role": "assistant", "content": "Ask your queries related to MSC Publications"}]
+
 def KMM_templates():
     st.session_state.messages = [{"role": "assistant", "content": "Ask your queries related to KMM Templates"}]
 
@@ -77,19 +82,26 @@ def clear_history():
 
 def user_input(user_question):
     embeddings = GoogleGenerativeAIEmbeddings(
-        model="models/embedding-001", google_api_key=google_api_key) 
+        model="models/embedding-001", google_api_key=google_api_key
+    )
 
-    new_db = FAISS.load_local("faiss_index", embeddings, allow_dangerous_deserialization=True) 
+    new_db = FAISS.load_local("faiss_index", embeddings, allow_dangerous_deserialization=True)
     docs = new_db.similarity_search(user_question)
 
     chain = get_conversational_chain()
 
-    response = chain(
-        {"input_documents": docs, "question": user_question}, return_only_outputs=True,request_options={"timeout": 1000}, )
+    try:
+        response = chain(
+            {"input_documents": docs, "question": user_question},
+            return_only_outputs=True,
+            request_options={"timeout": 1000},
+        )
+    except DeadlineExceeded:
+        logger.error("Request to Google Generative AI timed out.")
+        st.error("Request to Google Generative AI timed out. Please try again later.")
+        return None
 
     return response
-
-
 
 def clear_text():
     st.session_state.my_text = st.session_state.widget
@@ -106,33 +118,30 @@ def main():
         pdf_files = [os.path.join(pdf_folder_path, f) for f in os.listdir(pdf_folder_path) if f.endswith('.pdf')]
         raw_text = get_pdf_text(pdf_files)
         text_chunks = get_text_chunks(raw_text)
-        #st.markdown(f"Len chunks: {len(text_chunks)}")
-        #print (len(text_chunks))
         get_vector_store(text_chunks)
-       #st.success("PDF files processed successfully")
     else:
         st.error("Invalid folder path!")
+
     col1, col2, col3 = st.columns(3)
 
     with col1:
         st.write(' ')
 
     with col2:
-        st.image(r"Intro.png",width=300)
+        st.image(r"Intro.png", width=300)
 
     with col3:
         st.write(' ')
 
     # Main content area for displaying chat messages
-    
     st.markdown('<div style="background-color:#FFA500;padding:10px;border-radius:10px;">'
-            '<h2 style="color:#214761;text-align:center;font-size:20px;">Hi there! I am MSC Genie. I am a GenAI Bot that will be answering your queries. Currently, the following repositories are covered.</h2>'
-            '</div>', unsafe_allow_html=True)
+                '<h2 style="color:#214761;text-align:center;font-size:20px;">Hi there! I am MSC Genie. I am a GenAI Bot that will be answering your queries. Currently, the following repositories are covered.</h2>'
+                '</div>', unsafe_allow_html=True)
 
     st.sidebar.button('Clear Chat History', on_click=clear_history, key="clear_button")
 
     # Layout for buttons in three columns
-    col1, col2, col3 = st.columns(3)  
+    col1, col2, col3 = st.columns(3)
 
     with col1:
         if st.button('MSC Publications', on_click=MSC_publications, key="MSC_Publications", disabled=True):
@@ -141,7 +150,7 @@ def main():
     with col2:
         if st.button('HR Policies', on_click=HR_Policies, key="HR_Policies"):
             HR_Policies()
-            
+
     with col3:
         if st.button("KMM Templates", on_click=KMM_templates, key="KMM_Templates", disabled=True):
             KMM_templates()
@@ -149,43 +158,39 @@ def main():
     # Chat input and message display
     if "messages" not in st.session_state:
         st.session_state.messages = []
-   
 
     # Display chat messages
     for message in st.session_state.messages:
         if message["role"] == "user":
             st.image("user.png", width=50)
             st.markdown(
-            f'<div class="st-emotion-cache-4oy321" style="background-color: #D3E0EA; padding: 10px; border-radius: 10px; text-align: left;">{message["content"]}</div>',
-           unsafe_allow_html=True
-       )
+                f'<div class="st-emotion-cache-4oy321" style="background-color: #D3E0EA; padding: 10px; border-radius: 10px; text-align: left;">{message["content"]}</div>',
+                unsafe_allow_html=True
+            )
         elif message["role"] == "assistant":
             cols = st.columns(8)
-            with cols[7]: 
-                st.image("Found.png", width=75)            
-            
+            with cols[7]:
+                st.image("Found.png", width=75)
+
             st.markdown(
-            f'<div class="message-container" style="display: flex; justify-content: flex-end;">'
-            f'<div class="st-emotion-cache-4oy321" style="background-color: #C4E4F7; padding: 10px; border-radius: 10px; text-align: right;">{message["content"]}</div>'
-            
-            f'</div>',
-            unsafe_allow_html=True
-        )
+                f'<div class="message-container" style="display: flex; justify-content: flex-end;">'
+                f'<div class="st-emotion-cache-4oy321" style="background-color: #C4E4F7; padding: 10px; border-radius: 10px; text-align: right;">{message["content"]}</div>'
+                f'</div>',
+                unsafe_allow_html=True
+            )
 
     # User input prompt at the end
     user_query = st.chat_input("Enter your query")
 
     if user_query:
         st.session_state.messages.append({"role": "user", "content": user_query})
-    
         response = user_input(user_query)
         if response is not None:
             if isinstance(response['output_text'], list):
                 for response_item in response['output_text']:
                     st.session_state.messages.append({"role": "assistant", "content": response_item})
-                
             else:
                 st.session_state.messages.append({"role": "assistant", "content": response["output_text"]})
-                    
+
 if __name__ == "__main__":
     main()
